9d1dc57b34317b1b277b77a9b1eeedec569c14fa
Sham kamani <shamkamani48@gmail.com>
2026-02-18 22:47:19 +0500

    Add files via upload

---
+++ Task2/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'screens/quote_screen.dart';
+
+void main() {
+  runApp(const QuoteApp());
+}
+
+class QuoteApp extends StatelessWidget {
+  const QuoteApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(useMaterial3: true),
+      home: const QuoteScreen(),
+    );
+  }
+}
