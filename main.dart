54a81a09c1d2abafcbbd5f0f3b2e5535266a304d
Sham kamani <shamkamani48@gmail.com>
2026-02-18 21:25:34 +0500

    Task1 (Flashcard quiz app

---
+++ main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
+
+void main() {
+  runApp(const FlashcardApp());
+}
+
+class FlashcardApp extends StatelessWidget {
+  const FlashcardApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flashcard Quiz',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+          brightness: Brightness.light,
+          surface: const Color(0xFFF3F0FF),
+        ),
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
